3b166e739c178e15150d189b0675474a7398d9da
Abu Zaid Khan <abuzaidkhan99@gmail.com>
2021-12-27 19:07:43 -0500

    first draft

---
+++ scraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+story = ""
+depth = 3
+base_url = "https://en.wikipedia.org"
+search = "/wiki/United_States"
+
+for d in range(depth):
+    topic = search[6:].replace("_"," ")
+    print(topic)
+    result = requests.get(base_url+search)
+    page = BeautifulSoup(result.text, "html.parser")
+    content = page.find(class_="mw-parser-output")
+    search_para_parent = content.find("b", text=topic).parent
+    search = search_para_parent.find_all("a")[-1]['href']
+    for line in search_para_parent.strings:
+        story += line
+    story += "\n"
+    print(story)
+    story = ""
+
+#print(story)
+
+
+#bold = content.find("b")
+#parent_bold = bold.parent
+#last_link = parent_bold.find_all("a")
+#print("search=",last_link[-1]['href'])
